b0cf9686d6b2e4811036643cb7bb8e3c1e2a3568
Ronaldo Cândido dos Santos Sobrinho <rcss@ic.ufal.br>
2026-07-29 20:43:12 -0300

    Update PcAlHub.tsx

--- src/pages/ConcursoPoliciaCivil/PcAlHub.tsx
+++ src/pages/ConcursoPoliciaCivil/PcAlHub.tsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import { PcAlAgente2026Page } from './components/PcAlAgente2026Page';
-import { PcAlEscrivao2026Page } from './components/PcAlEscrivao2026Page';
 import './style.css';
 
 export default function PcAlHub() {
