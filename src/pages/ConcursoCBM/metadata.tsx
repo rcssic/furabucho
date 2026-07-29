a56f54147b3bcc14503369167a7cb3ed17e4cdd5
Ronaldo Cândido dos Santos Sobrinho <rcss@ic.ufal.br>
2026-07-29 16:16:53 -0300

    Update metadata.tsx

--- src/pages/ConcursoCBM/metadata.tsx
+++ src/pages/ConcursoCBM/metadata.tsx
@@ -15,9 +15,6 @@ export default {
     ],
     icon: (
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-fire-extinguisher-icon lucide-fire-extinguisher"><path d="M15 6.5V3a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v3.5"/><path d="M9 18h8"/><path d="M18 3h-3"/><path d="M11 3a6 6 0 0 0-6 6v11"/><path d="M5 13h4"/><path d="M17 10a4 4 0 0 0-8 0v10a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2Z"/>
-            <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
-            <line x1="8" y1="21" x2="16" y2="21"></line>
-            <line x1="12" y1="17" x2="12" y2="21"></line>
         </svg>
     ),
 };
