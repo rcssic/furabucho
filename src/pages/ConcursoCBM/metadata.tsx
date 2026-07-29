e80b9eb70c3de0763f7fe07befb55013c2ea7eea
Ronaldo Cândido dos Santos Sobrinho <rcss@ic.ufal.br>
2026-07-29 14:33:46 -0300

    Create metadata.tsx

---
+++ src/pages/ConcursoCBM/metadata.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default {
+    id: "concurso-CMB",
+    title: "Concurso CBM",
+    description: "Página com todas as informações do concurso SEPLAG AL 2026 para o Cargo 2 (Análise de Sistemas).",
+    keywords: [
+        "concurso",
+        "CBM",
+        "alagoas",
+        "analista",
+        "sistemas",
+        "ti",
+        "edital"
+    ],
+    icon: (
+        <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
+            <line x1="8" y1="21" x2="16" y2="21"></line>
+            <line x1="12" y1="17" x2="12" y2="21"></line>
+        </svg>
+    ),
+};
